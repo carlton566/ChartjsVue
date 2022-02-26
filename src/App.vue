135c9b426b522cf34ae6f53f2d0ec6e5827cf2be
Carlton Mascarenhas <mascarenhascarlton56789@gmail.com>
2022-02-26 15:33:36 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+<div id="app">
+    <PlanetChart/>
+    <Dropdown/>
+  </div>
+</template>
+
+<script>
+import PlanetChart from './components/PlanetChart.vue'
+import Dropdown from './components/HelloWorld.vue'
+
+
+
+export default {
+  name: 'App',
+  components: {
+    PlanetChart,
+    Dropdown
+    
+  }
+}
+</script>
+
+
---
+++ src/components/PlanetChart.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="ui two column centered grid">
+  <div class="row">
+        <div class="column">
+            <canvas id="planet-chart"></canvas>
+        </div>
+        <div class="column">
+            <canvas id="planet-chart1"></canvas>
+        </div>
+  </div>
+  <div class="row">
+        <div class="column">
+            <canvas id="planet-chart2"></canvas>
+        </div>
+        <div class="column">
+            <canvas id="planet-chart3"></canvas>
+        </div>
+  </div>
+</div>
+
+  
+</template>
+
+<script>
+
+import Chart from 'chart.js'
+import { planetChartData, planetChartData1, planetChartData2, planetChartData3  } from '../assets/js/planet-data.js'
+
+export default {
+  name: 'PlanetChart',
+  data() {
+    return {
+      planetChartData: planetChartData,
+      planetChartData1:planetChartData1,
+      planetChartData2:planetChartData2,
+      planetChartData3:planetChartData3
+    }
+  },
+  mounted() {
+    const ctx = document.getElementById('planet-chart');
+    const ctx1 = document.getElementById('planet-chart1');
+    const ctx2 = document.getElementById('planet-chart2');
+    const ctx3 = document.getElementById('planet-chart3');
+    new Chart(ctx, this.planetChartData);
+    new Chart(ctx1, this.planetChartData1);
+    new Chart(ctx2, this.planetChartData2);
+    new Chart(ctx3, this.planetChartData3);
+  }
+}
+</script>
